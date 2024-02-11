0f2919b1a3f0166655b26041ccaa97ad8a705f09
Sotiris Tsiamouris <tsot85@gmail.com>
2024-02-11 19:08:23 +0200

    fix: skaffold and production build in progress

--- packages/products/src/routes/v1/index.ts
+++ packages/products/src/routes/v1/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/require-await */
 import { FastifyPluginAsync, FastifyPluginOptions } from "fastify";
 import {
   readOneOptions,
--- packages/products/vite.config.ts
+++ packages/products/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { resolve } from "path";
 
 export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "products",
+      fileName: "index",
+    },
+  },
   plugins: [
     tsconfigPaths({
       parseNative: false,
